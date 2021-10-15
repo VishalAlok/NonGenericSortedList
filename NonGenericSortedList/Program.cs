f9f94ee2a030bc922b2508623ad85ae0bff7efb1
vishal.Alok <you@example.com>
2021-10-15 06:35:33 +0530

    Add project files.

---
+++ NonGenericSortedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList obj = new SortedList();
+            obj.Add("Vishal", "Bangalore");
+            obj.Add("Abhay", "Chennai");
+            obj.Add("Rahul", "Hyderabad");
+            obj.Add("Ravi", "Pune");
+            foreach (string s in obj.Keys)
+            {
+                Console.Write(s + " " + obj[s] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
